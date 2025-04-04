d20665f21d2a14c90765035771fdf1cb2b1126f9
Plinio Gomes <plinioigt@gmail.com>
2025-04-04 16:09:41 -0300

    added selled jdbc dao logic

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,9 +11,10 @@ public class Main {
     public static void main(String[] args) {
         Department dep = new Department(1, "Books");
 
-        Seller seller = new Seller(1, "Joao", "joao@joao.com", new Date(), 3000.0, dep );
 
         ISellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
     }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implementation.SellerDaoJDBC;
 
 public class DaoFactory {
 
     //gof pattern factory?
     public static ISellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
