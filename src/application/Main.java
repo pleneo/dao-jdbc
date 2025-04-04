e1e539761162d1e9e2084b0fde98814838d78ddd
Plinio Gomes <plinioigt@gmail.com>
2025-04-04 14:53:28 -0300

    added seller class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        Department dep = new Department(1, "Books");
 
-        
+        Seller seller = new Seller(1, "Joao", "joao@joao.com", new Date(), 3000.0, dep );
+
+        System.out.println(seller);
+
     }
 }
